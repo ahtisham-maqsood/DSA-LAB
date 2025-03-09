d9f2a16831928caa788661bce8e1fcf117a2034c
ahtisham-maqsood <maqsoodahtisham59@gmail.com>
2025-03-09 12:18:34 +0500

    Add files via upload

---
+++ LAB3/DSAlab3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;      
+    Node* next;     
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+class LinkedList {
+private:
+    Node* head;    
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+    void insertAtFront(int);    
+    void insertAtEnd(int);
+    void printList();
+};
+
+void LinkedList::insertAtFront(int value) {
+    Node* newNode = new Node(value);
+    newNode->next = head;
+    head = newNode;
+}
+void LinkedList::insertAtEnd(int value) {
+    Node* newNode = new Node(value);
+
+    if (head == nullptr) { 
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != nullptr) { 
+        temp = temp->next;
+    }
+
+    temp->next = newNode; 
+}
+void LinkedList::printList() {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "nullptr" << endl;
+}
+int main() {
+    LinkedList list;
+    list.insertAtFront(2);
+    list.insertAtEnd(14);
+    cout << "LinkedList: ";
+    list.printList();
+
+    return 0;
+}
